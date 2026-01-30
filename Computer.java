db00079269b994f128d5c0fed4cb8a61fab96e80
Daouda-sudo <ndaw.daoudadibocor.studente@itispaleocapa.it>
2026-01-30 12:12:15 +0000

    programma su computer e liste.

---
+++ Computer.java
@@ -0,0 +1,18 @@
+public class Computer {
+    String marca;
+    String processore;
+    double capacitaRam;
+    
+    public Computer(String marca, String processore, double capacitaRam) {
+        this.marca = marca;
+        this.processore = processore;
+        this.capacitaRam = capacitaRam;
+    }
+
+    //TODO getter
+
+
+    public String getMarca(){
+
+    }
+}
